cbd3b531ab6135c27e69e06eb89cd2e051622b85
Duppy-Conqueror <duppyconqueror206@gmail.com>
2023-12-21 21:49:09 +0800

    Trimmed down Day11

--- src/Day11.java
+++ src/Day11.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 class Position {
@@ -18,9 +17,6 @@ class Position {
 }
 
 public class Day11 {
-
-
-
     public static List<Integer> emptyRows(List<String> lines) {
         List<Integer> emptyRows = new ArrayList<>();
         for (int i = 0; i < lines.size(); ++i) {
@@ -45,41 +41,15 @@ public class Day11 {
         return emptyColumns;
     }
 
-    public static List<Position> locateGalaxiesPartOne(List<String> lines, List<Integer> emptyRows, List<Integer> emptyColumns) {
-        List<String> expandedUniverse = new ArrayList<>(lines);
-        for (int i = emptyRows.size() - 1; i > -1; --i) {
-            expandedUniverse.add(emptyRows.get(i), expandedUniverse.get(emptyRows.get(i)));
-        }
-        for (int i = 0; i < expandedUniverse.size(); ++i) {
-            String temp = expandedUniverse.get(i);
-            for (int j = emptyColumns.size() - 1; j > -1; --j) {
-                temp = temp.substring(0, emptyColumns.get(j)) + "." + temp.substring(emptyColumns.get(j));
-            }
-            expandedUniverse.set(i, temp);
-        }
-        List<Position> galaxies = new ArrayList<>();
-        for (int row = 0; row < expandedUniverse.size(); ++row) {
-            String line = expandedUniverse.get(row);
-            for (int col = 0; col < line.length(); ++col) {
-                if (line.charAt(col) == '#') {
-                    galaxies.add(new Position(row, col));
-                }
-            }
-        }
-        return galaxies;
-    }
-
-    public static List<Position> locateGalaxiesPartTwo(List<String> lines, List<Integer> emptyRows, List<Integer> emptyColumns) {
+    public static List<Position> locateGalaxies(List<String> lines, List<Integer> emptyRows, List<Integer> emptyColumns, long times) {
         List<Position> galaxies = new ArrayList<>();
         for (int row = 0; row < lines.size(); ++row) {
             String line = lines.get(row);
             for (int col = 0; col < line.length(); ++col) {
                 if (line.charAt(col) == '#') {
-                    int finalRow = row;
-                    int finalColumn = col;
-                    long offsetRow = row + 999999L * emptyRows.stream().filter(x -> x < finalRow).toList().size();
-                    long offsetCol = col + 999999L * emptyColumns.stream().filter(x -> x < finalColumn).toList().size();
-                    galaxies.add(new Position(offsetRow, offsetCol));
+                    final int finalRow = row;
+                    final int finalColumn = col;
+                    galaxies.add(new Position(finalRow + (times - 1) * emptyRows.stream().filter(x -> x < finalRow).toList().size(), finalColumn + (times - 1) * emptyColumns.stream().filter(x -> x < finalColumn).toList().size()));
                 }
             }
         }
@@ -89,7 +59,7 @@ public class Day11 {
     public static long partOne(List<String> lines) {
         List<Integer> emptyRows = emptyRows(lines);
         List<Integer> emptyColumns = emptyColumns(lines);
-        List<Position> galaxies = locateGalaxiesPartOne(lines, emptyRows, emptyColumns);
+        List<Position> galaxies = locateGalaxies(lines, emptyRows, emptyColumns, 2);
         long steps = 0;
         for (int i = 0; i < galaxies.size(); ++i) {
             for (int j = i + 1; j < galaxies.size(); ++j) {
@@ -102,7 +72,7 @@ public class Day11 {
     public static long partTwo(List<String> lines) {
         List<Integer> emptyRows = emptyRows(lines);
         List<Integer> emptyColumns = emptyColumns(lines);
-        List<Position> galaxies = locateGalaxiesPartTwo(lines, emptyRows, emptyColumns);
+        List<Position> galaxies = locateGalaxies(lines, emptyRows, emptyColumns,1000000);
         long steps = 0;
         for (int i = 0; i < galaxies.size(); ++i) {
             for (int j = i + 1; j < galaxies.size(); ++j) {
